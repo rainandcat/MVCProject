491af95b0d796221d6863494a3a8b8c8f037122f
Tsung Lee <ssee11tw@gmail.com>
2021-01-04 01:56:12 +0800

    feat:取得Category Image功能

---
+++ MVCProject.Service/ViewModels/CategoriesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCProject.Service.ViewModels
+{
+    public class CategoriesViewModel
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        public Byte[] Picture { get; set; }
+    }
+}
--- MVCProject.Web/Controllers/CategoryController.cs
+++ MVCProject.Web/Controllers/CategoryController.cs
@@ -20,5 +20,20 @@ namespace MVCProject.Web.Controllers
             var result = _service.categoryService.GetAll();
             return View(result);
         }
+
+        public IActionResult GetImage(int id)
+        {
+            var category = _service.categoryService.GetByID(id);
+            if (category != null && category.Picture != null)
+            {
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    byte[] image = category.Picture;
+                    ms.Write(image, 78, image.Length - 78);
+                    return File(ms.ToArray(), "image/jpeg");
+                }
+            }
+            return new EmptyResult();
+        }
     }
 }
