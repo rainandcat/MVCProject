84e591cae8c50f8a7ba9e041da6e2e9eb4ce2821
Tsung Lee <ssee11tw@gmail.com>
2021-01-04 01:12:01 +0800

    feat:建立BaseController

---
+++ MVCProject.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Web.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public readonly BaseDbContext db;
+
+        protected BaseController(BaseDbContext db)
+        {
+            this.db = db;
+        }
+    }
+}
---
+++ MVCProject.Web/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Web.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        public CategoryController(BaseDbContext db) : base(db)
+        {
+        }
+    }
+}
